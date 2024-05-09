d81c7a2abb6d7f697be32fc82ba9dac1d4497567
ArkjuniorK <created.arkjuniork@gmail.com>
2024-05-09 12:44:41 +0800

    refactor: change rules for symbol to use regex instead of manual defined characters

--- lisp/parsing.c
+++ lisp/parsing.c
@@ -48,9 +48,7 @@ int main(int argc, char **argv)
     mpca_lang(MPCA_LANG_DEFAULT,
               "                                                           \
                 number : /-?[0-9]+/ ;                                     \
-                symbol : '+' | '-' | '*' | '/' | '^' | '%'                \
-                       | \"list\" | \"head\" | \"tail\" | \"join\"        \
-                       | \"eval\" | \"cons\" | \"len\" | \"init\" ;       \
+                symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/                 \
                 sexpr  : '(' <expr>* ')' ;                                \
                 qexpr  : '{' <expr>* '}' ;                                \
                 expr   : <number> | <symbol> | <sexpr> | <qexpr> ;        \
